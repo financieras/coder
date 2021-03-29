6a5cd488b1a66f8dfc108e28174d802c65c9c92b
addolfo <financieras@gmail.com>
2021-03-29 02:20:02 +0200

    función calculadora con una función que suma

--- calculadora.py
+++ calculadora.py
@@ -1,6 +1,16 @@
 import sys
-nombre = sys.argv[0]
-valor1 = sys.argv[1]
-valor2 = sys.argv[2]
-#print(nombre)
-print(int(valor1) + int(valor2))
+
+valor1 = int(sys.argv[1])
+valor2 = int(sys.argv[2])
+# para ejecutar tecleamos en la terminal:
+# python3 calculadora.py 2 3
+
+
+def suma(numero1, numero2):
+    resultado = numero1 + numero2
+    return resultado
+
+# llamamos a la función suma 
+# y le pasamos los dos parámetros
+print(suma(valor1,valor2))
+
